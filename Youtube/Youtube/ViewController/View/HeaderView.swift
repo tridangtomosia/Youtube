d9d6aa13e9df0387355eb4f9361ef8f03ce6fde9
manh_tomosia <manh.nghiem@tomosia.com>
2020-02-03 15:49:46 +0700

    new update

---
+++ Youtube/Youtube/ViewController/View/HeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderView.swift
+//  Youtube
+//
+//  Created by apple on 2/3/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class HeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Youtube/Youtube/ViewController/View/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  Youtube
+//
+//  Created by apple on 2/3/20.
+//  Copyright © 2020 apple. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
